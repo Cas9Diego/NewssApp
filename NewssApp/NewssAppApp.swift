e1a39a357daf7e454357d1eb010c386c15dd65ff
Cas9Diego <DaCastro@outlook.com>
2022-08-29 17:12:49 -0500

    Initial Commit

---
+++ NewssApp/NewssAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewssAppApp.swift
+//  NewssApp
+//
+//  Created by Diego Castro on 29/08/22.
+//
+
+import SwiftUI
+
+@main
+struct NewssAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
